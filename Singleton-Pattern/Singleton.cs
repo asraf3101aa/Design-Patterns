22dc3dff55aed555a2e214094106f9c10604a232
asraf3101aa <asraf3101aa@gmail.com>
2023-04-30 13:00:32 +0545

    Added singleton pattern

---
+++ Singleton-Pattern/Singleton.cs
@@ -0,0 +1,19 @@
+﻿public sealed class Singleton
+{
+    private Singleton() { }
+    private static Singleton _instance;
+    public static Singleton GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Singleton();
+        }
+        return _instance;
+    }
+
+    public void Show()
+    {
+        //something to do
+    }
+}
+
